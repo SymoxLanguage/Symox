eef371d0dfe57e21d9f2c957476b3c0758b8fe13
astrodude23342 <92599924+astrodude23342@users.noreply.github.com>
2022-04-02 11:12:50 +0200

    1.2.1

--- shell.py
+++ shell.py
@@ -2,7 +2,7 @@ import basic
 
 while True:
 		text = input('symox >')
-		result, error = basic.run('<stdin>', text)
+		result, error = basic.run(text)
 
 		if error: print(error.as_string())
 		else: print(result)
