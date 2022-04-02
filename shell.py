c0b3802fe9b910bfb2816cbc98a27fce36476f76
astrodude23342 <92599924+astrodude23342@users.noreply.github.com>
2022-04-02 16:54:25 +0200

    Update shell.py

--- shell.py
+++ shell.py
@@ -1,8 +1,8 @@
 import basic
 
 while True:
-		text = input('symox >')
-		result, error = basic.run('<stdin>', text)
+	text = input('symox > ')
+	result, error = basic.run('<stdin>', text)
 
-		if error: print(error.as_string())
-		elif result: print(result)
+	if error: print(error.as_string())
+	elif result: print(result)
